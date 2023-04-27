83a45dd36150b24510963b4b443d8d9e4628a5d3
Adewole Conde <phatboislym@protonmail.com>
2023-04-27 11:28:38 +0100

    add: task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,14 +2,35 @@
 
 """
 contains:
+    class `RedactingFormatter`
     function `filter_datum`
 """
 
 
 import re
+import logging
+from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str, message: str,
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ function scrubs selected PII data fields with regex"""
     return re.sub('|'.join('(?<={}=).*?(?={})'.format(field, separator)
