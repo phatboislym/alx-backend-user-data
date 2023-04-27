e8f1627e06a9211e3b4ff026619d46b9c3ef3a5d
Adewole Conde <phatboislym@protonmail.com>
2023-04-27 22:33:30 +0100

    add: task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,15 +5,32 @@ contains:
     class `RedactingFormatter`
     function `filter_datum`
     function `get_logger`
+    function `get_db`
 """
 
-
-import re
 import logging
+import mysql.connector
+from os import environ
+import re
 from typing import List
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS: tuple = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    function scrubs selected PII data fields with regex
+    args:   fields: List[str]
+            redaction: str
+            message: str
+            separator: str
+    return: cleaned: str
+    """
+    cleaned = re.sub('|'.join('(?<={}=).*?(?={})'.format(field, separator)
+                              for field in fields), redaction, message)
+    return (cleaned)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -28,10 +45,14 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ filter values in incoming log records using filter_datum """
+        """
+        filter values in incoming log records using filter_datum
+        args:   record: logging.LogRecord
+        return: str
+        """
         record.msg = filter_datum(
             self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super().format(record)
+        return (super().format(record))
 
 
 def get_logger() -> logging.Logger:
@@ -48,12 +69,19 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
+    return (logger)
 
-    return logger
 
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    args:   None
+    return: connector: mysql.connector.connection.MySQLConnection
+    """
+    username = environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+    host = environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = environ.get('PERSONAL_DATA_DB_NAME')
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ function scrubs selected PII data fields with regex"""
-    return re.sub('|'.join('(?<={}=).*?(?={})'.format(field, separator)
-                           for field in fields), redaction, message)
+    connector = mysql.connector.connect(user=username, password=password,
+                                        host=host, database=database)
+    return (connector)
