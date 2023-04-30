3817f894025a65bb47cab3d507ed74c5e9bcfde7
Adewole Conde <phatboislym@protonmail.com>
2023-04-30 01:34:53 +0100

    add: task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,6 +3,7 @@
 """
 contains:
     function `hash_password`
+    function `is_valid`
 """
 
 import bcrypt
@@ -17,3 +18,15 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed: bytes = bcrypt.hashpw(password_bytes, salt)
     return (hashed)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    args:   hashed_password: bytes
+            password: str
+    return: True | False: bool
+    """
+    password_bytes: bytes = password.encode('utf-8')
+    if bcrypt.checkpw(password_bytes, hashed_password):
+        return True
+    return False
