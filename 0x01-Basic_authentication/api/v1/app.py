75484a01266e06884ea5d1aa74fa7c02364c74fa
Adewole Conde <phatboislym@protonmail.com>
2023-05-02 18:17:05 +0100

    fix: revert changes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,12 +7,9 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from flask_lazyviews import LazyViews
 
 
 app = Flask(__name__)
-app.config["LAZYVIEWS_CACHE_TIMEOUT"] = 60
-lazyviews = LazyViews(app)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
@@ -27,4 +24,4 @@ def not_found(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
