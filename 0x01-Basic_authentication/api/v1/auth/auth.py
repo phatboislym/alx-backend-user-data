0b5d1dc167ebddd1de29bf4643150d07c5a7eca5
Adewole Conde <phatboislym@protonmail.com>
2023-05-03 15:59:17 +0100

    fix: remove tests

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -63,14 +63,3 @@ class Auth():
         return: User: TypeVar('User')
         """
         return None
-
-
-a = Auth()
-
-print(a.require_auth(None, None))
-print(a.require_auth(None, []))
-print(a.require_auth("/api/v1/status/", []))
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
