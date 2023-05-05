d2e4689c95abde637b61c25062de2599708a662a
Adewole Conde <phatboislym@protonmail.com>
2023-05-05 14:29:40 +0100

    add: task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ contains:
     class `BasicAuth`
 """
 from api.v1.auth.auth import Auth
+from typing import Optional
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,21 @@ class BasicAuth(Auth):
 
     def __init__(self) -> None:
         pass
+
+    def extract_base64_authorization_header(self, authorization_header: str)
+    -> Optional[str]:
+        """
+        returns the Base64 part of the Authorization header
+        args:   self
+                authorization_header: str
+        return: Optional[str]
+        """
+        base64_header: Optional[str] = None
+        auth_header = authorization_header
+        if (auth_header is None) or (type(auth_header) != str):
+            return (base64_header)
+        elif authorization_header.startswith("Basic "):
+            base64_header = authorization_header[6:]
+            return (base64_header)
+        else:
+            return (base64_header)
