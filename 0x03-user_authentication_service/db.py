de4127b5eaead6d1d862fa1f249d2dcefa0f8ec0
Adewole Conde <phatboislym@protonmail.com>
2023-05-09 21:34:24 +0100

    fix: change to `echo=False`

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,16 +3,11 @@
 """
 DB module
 """
-from logging import getLogger, WARNING
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
-
-
-getLogger('sqlalchemy.engine').propagate = False
-getLogger('sqlalchemy.engine').setLevel(WARNING)
 
 
 class DB:
@@ -24,7 +19,7 @@ class DB:
         """
         initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
