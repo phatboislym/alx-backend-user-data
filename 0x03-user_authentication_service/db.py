44a610b537cfb465ca04c5c6ec44d38d1e3fc773
Adewole Conde <phatboislym@protonmail.com>
2023-05-09 21:21:53 +0100

    add: task 1

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""
+DB module
+"""
+from logging import getLogger, WARNING
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User
+
+
+getLogger('sqlalchemy.engine').propagate = False
+getLogger('sqlalchemy.engine').setLevel(WARNING)
+
+
+class DB:
+    """
+    DB class
+    """
+
+    def __init__(self) -> None:
+        """
+        initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        memoized session object
+        args:   self
+        return: Session
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        adds a user to the database
+        args:   self
+                email: str
+                hashed_password: str
+        return: user: User
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
