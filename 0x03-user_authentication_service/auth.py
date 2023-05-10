b3d23ea9cfbfbf64ebb72f335bdfaca8e58530cf
Adewole Conde <phatboislym@protonmail.com>
2023-05-10 18:28:56 +0100

    add: task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,9 +4,12 @@
 module for auth
 contains:
     function `_hash_password`
+    class `Auth`
 """
 
 import bcrypt
+from db import DB, NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,6 +19,34 @@ def _hash_password(password: str) -> bytes:
     return: bytes_string: bytes
     """
     password_bytes: bytes = password.encode('utf-8')
-    salt = bcrypt.gensalt()
+    salt: bytes = bcrypt.gensalt()
     hashed: bytes = bcrypt.hashpw(password_bytes, salt)
     return (hashed)
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database
+    methods:    class constructor
+                register_user
+    """
+
+    def __init__(self) -> None:
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register a new user
+        args:   self
+                email: str
+                password: str
+        return: user: User
+        """
+        try:
+            user: User = self._db.find_user_by(email=email)
+            raise ValueError(f"User {user.email} already exists")
+        except NoResultFound:
+            hashed_password: bytes = _hash_password(password)
+            user: User = self._db.add_user(
+                email=email, hashed_password=hashed_password)
+            return user
