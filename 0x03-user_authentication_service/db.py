0e7fb1b6728aa59941449ad099f2af761778e453
Adewole Conde <phatboislym@protonmail.com>
2023-05-10 15:51:57 +0100

    fix: change NoResultFound import

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,9 +4,10 @@
 DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
