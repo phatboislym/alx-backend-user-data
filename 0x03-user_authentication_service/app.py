c3052ee20c37ce0fa7a9b626bc2f8a0f32d95c0a
Adewole Conde <phatboislym@protonmail.com>
2023-05-10 18:50:55 +0100

    add: task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+module for Flask app
+"""
+from flask import Flask, jsonify, Response
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def test_route() -> Response:
+    """
+    args:   None
+    return: payload: Response
+    """
+    payload: Response = jsonify({"message": "Bienvenue"})
+    print(type(payload))
+    return payload
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
