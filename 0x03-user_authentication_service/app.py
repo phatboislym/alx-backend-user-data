e74863c80cb4bea3ab0452dd1268fe9da6838d58
Adewole Conde <phatboislym@protonmail.com>
2023-05-11 01:08:46 +0100

    add: task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,8 +3,12 @@
 """
 module for Flask app
 """
-from flask import Flask, jsonify, Response
+from auth import Auth
+from flask import Flask, jsonify, Response, request
+from typing import Optional, Tuple
+from user import User
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -18,5 +22,26 @@ def test_route() -> Response:
     return payload
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> Tuple[Response, Optional[int]]:
+    """
+    end-point to register a user
+    args:   email: str
+            password: str
+    return: payload: Response
+    """
+    email: str = request.form.get('email')
+    password: str = request.form.get('password')
+    try:
+        user: User = AUTH.register_user(email=email, password=password)
+        user_created: Response = jsonify(
+            {"email": "<registered email>", "message": "user created"})
+        return user_created
+    except ValueError:
+        user_exists: Response = jsonify(
+            {"message": "email already registered"})
+        return (user_exists, 400)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
